2804a4c398bd66db2e80bde5c7d7ce0a68b6de69
muhammad1749 <muhammadqodirmuminov2@gmail>
2022-12-17 11:45:26 +0500

    home page change

---
+++ js/admin.js
@@ -0,0 +1,149 @@
+import { findElement } from "./index.js";
+
+// variables
+
+const elInputTitle = findElement("#title"),
+	elForm = findElement(".form"),
+	elInputSubtitle = findElement("#subtitle"),
+	elInputImg = findElement("#image"),
+	elCheckbox = findElement("#checkbox"),
+	elSuccess = findElement("#succes"),
+	elPostDel = findElement(".main"),
+	elmodal = findElement(".modal"),
+	closebtn = findElement("#close-btn"),
+	closeModal = findElement("#close"),
+	elBtn = findElement("#btn");
+
+const templateAdmin = findElement("#admin-template"),
+	mainAdmin = findElement(".main"),
+	fragmentAdmin = document.createDocumentFragment();
+
+let posts = [];
+
+// Create post template
+
+elForm.addEventListener("submit", (evt) => {
+	evt.preventDefault();
+
+	const elTitle = elInputTitle.value,
+		elSubtitle = elInputSubtitle.value,
+		elImg = elInputImg.value.trim(),
+		isTop = !elCheckbox.checked;
+
+	fetch("https://639b4a2a31877e43d6888973.mockapi.io/posts", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			createdAt: "2022-12-15T15:14:16.982Z",
+			image: elImg,
+			name: elTitle,
+			subtitle: elSubtitle,
+			top: isTop,
+		}),
+	})
+		.then((res) => {
+			res.json();
+		})
+		.catch((data) => {
+			console.log(data);
+		});
+
+	elSuccess.classList.remove("d-none");
+	setTimeout(() => {
+		setInterval(() => {
+			elSuccess.classList.add("d-none");
+			elSuccess.style.transition = "all 0.6s ease-out ";
+			elForm.reset();
+		}, 500);
+	}, 1500);
+});
+
+// template posts
+
+fetch("https://639b4a2a31877e43d6888973.mockapi.io/posts")
+	.then((res) => res.json())
+	.then((data) => {
+		posts = data.reverse();
+		renderAdmin(posts);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+function renderAdmin(posts, parenttag = mainAdmin) {
+	parenttag.textContent = "";
+	posts.forEach((post) => {
+		const templateAdminPost = templateAdmin.content.cloneNode(true);
+
+		const title = templateAdminPost.querySelector("#title-body"),
+			img = templateAdminPost.querySelector("#img-body"),
+			subtitle = templateAdminPost.querySelector("#subtitle-body"),
+			date = templateAdminPost.querySelector("#date-body"),
+			isTop = templateAdminPost.querySelector("#isTop"),
+			delbtn = templateAdminPost.querySelector("#delete"),
+			dateNow = {
+				hour:
+					new Date().getHours() < 10
+						? "0" + new Date().getHours()
+						: new Date().getHours(),
+				minutes:
+					new Date().getMinutes() < 10
+						? "0" + new Date().getMinutes()
+						: new Date().getMinutes(),
+				date: new Date().getDate(),
+				month: new Date().getMonth(),
+				year: new Date().getFullYear(),
+			};
+
+		title.innerHTML = post.name;
+		img.src = post.image;
+		date.innerHTML = `${dateNow.hour}:${dateNow.minutes} / ${dateNow.date}.${dateNow.month}.${dateNow.year}`;
+		subtitle.innerHTML = post.subtitle;
+		isTop.textContent = `Is top post :  ${post.top}`;
+		delbtn.dataset.id = post.id;
+
+		fragmentAdmin.append(templateAdminPost);
+	});
+	mainAdmin.appendChild(fragmentAdmin);
+}
+
+// deleting a post
+
+elPostDel.addEventListener("click", (e) => {
+	if (e.target.matches("#delete")) {
+		const id = e.target.dataset.id;
+		modalActive();
+
+		elBtn.addEventListener("click", () => {
+			fetch(`https://639b4a2a31877e43d6888973.mockapi.io/posts/${id}`, {
+				method: "DELETE",
+			})
+				.then((data) => {
+					alert("removed succesfully");
+					location.reload();
+				})
+				.catch((err) => console.log(err));
+			modalRemove();
+		});
+		closebtn.addEventListener("click", () => {
+			modalRemove();
+		});
+		closeModal.addEventListener("click", () => {
+			modalRemove();
+		});
+	}
+});
+
+// modal section
+
+function modalActive() {
+	elmodal.style.display = "block";
+	document.body.style.overflow = "hidden";
+}
+
+function modalRemove() {
+	elmodal.style.display = "none";
+	document.body.style.overflow = "auto";
+}
---
+++ js/main.js
@@ -0,0 +1,92 @@
+import { findElement } from "./index.js";
+
+const templateTop = findElement("#post-template"),
+	fragment = document.createDocumentFragment(),
+	templateBodyPost = findElement("#body-template"),
+	fragmentBody = document.createDocumentFragment(),
+	parentEL = findElement(".cards"),
+	handlerEl = findElement("#article");
+
+let posts = [];
+let topPosts = [];
+
+fetch("https://639b4a2a31877e43d6888973.mockapi.io/posts")
+	.then((res) => res.json())
+	.then((data) => {
+		posts = data.reverse();
+		renderTop(posts);
+		renderbody(posts);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+function renderTop(posts, parenttag = parentEL) {
+
+	posts.forEach((item) => {
+		if (!item.top) {
+			topPosts.push(item);
+		}
+	});
+	for (let i = 0; i < 6; i++) {
+		const post = topPosts[i];
+
+		const templatePost = templateTop.content.cloneNode(true);
+
+		const title = templatePost.querySelector("#title"),
+			img = templatePost.querySelector("#img"),
+			date = templatePost.querySelector("#date"),
+			dateNow = {
+				hour: new Date().getHours(),
+				minutes: new Date().getMinutes(),
+				date: new Date().getDate(),
+				month: new Date().getMonth(),
+				year: new Date().getFullYear(),
+			};
+
+		title.innerHTML = post.name;
+		img.src = post.image;
+		date.innerHTML = `${dateNow.hour}:${dateNow.minutes} / ${dateNow.date}.${dateNow.month}.${dateNow.year}`;
+
+		fragment.append(templatePost);
+	}
+
+	parentEL.appendChild(fragment);
+}
+
+function renderbody(posts, parenttag = handlerEl) {
+	parenttag.textContent = "";
+
+	for (let i = 0; i < 6; i++) {
+		const post = posts[i];
+
+		const templateBody = templateBodyPost.content.cloneNode(true);
+
+		const title = templateBody.querySelector("#title-body"),
+			img = templateBody.querySelector("#img-body"),
+			subtitle = templateBody.querySelector("#subtitle-body"),
+			date = templateBody.querySelector("#date-body"),
+			dateNow = {
+				hour:
+					new Date().getHours() < 10
+						? "0" + new Date().getHours()
+						: new Date().getHours(),
+				minutes:
+					new Date().getMinutes() < 10
+						? "0" + new Date().getMinutes()
+						: new Date().getMinutes(),
+				date: new Date().getDate(),
+				month: new Date().getMonth(),
+				year: new Date().getFullYear(),
+			};
+
+		title.innerHTML = post.name;
+		img.src = post.image;
+		date.innerHTML = `${dateNow.hour}:${dateNow.minutes} / ${dateNow.date}.${dateNow.month}.${dateNow.year}`;
+		subtitle.innerHTML = post.subtitle;
+
+		fragmentBody.append(templateBody);
+	}
+
+	handlerEl.appendChild(fragmentBody);
+}
